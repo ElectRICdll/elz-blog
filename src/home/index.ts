08cb4884aa4ebd37420e1e116066babdcbec3ca5
ElectRICdll <2636422098@qq.com>
2024-06-24 16:17:37 +0800

    feat(home): new home page

---
+++ src/home/index.ts
@@ -0,0 +1,3 @@
+export { default as BlogApp } from './app-blog.vue'
+export { default as AboutPage } from './page-about.vue'
+export { default as HomePage } from './page-home.vue'
---
+++ src/home/page-home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="welcome-container flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800"
+  >
+    <h1 class="text-4xl font-bold mb-4">博客</h1>
+    <p class="text-xl mb-6">本博客十分欢迎您的到来！</p>
+    <button
+      @click="$router.push('/posts')"
+      class="blog-border px-4 py-2 bg-primary text-white rounded hover:opacity-70 transition"
+    >
+      点击进入
+    </button>
+  </div>
+</template>
